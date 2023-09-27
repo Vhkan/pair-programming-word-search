dea2898f4cc2fe3004033cd431daf9a6338c9fe3
Vhkan <vhkan@yahoo.com>
2023-09-27 16:55:35 -0600

    Adding wordSearch PP project files

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,46 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = require("./matrix_transposition");
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  if (word === "") {
+    return false;
+  }
+
+  if (letters.length === 0) {
+    return false;
+  }
+
+  //["h","e","l","l","o"] <--if we call join here,
+  //"hello" <---after join
+
+  //after map is finished completely
+  //horizontal join looks like this
+  [
+    "hello",
+    "world",
+    "vadym"
+  ];
+
+  //this is for searching horizontally
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  //do the same thing for this matrix
+  const newMatrix = transpose(letters);
+
+  //this ALSO searches horizontally, BUT it's on a newly transposed matrix
+  const verticalJoin = newMatrix.map(ls => ls.join(''));
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  //console.log(newMatrix);
+
+  //return false if worsdSearch cannot find anything at all
+  return false;
+};
+
+module.exports = wordSearch;
+
+
